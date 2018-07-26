4e0b4f596d43f96a93eb923269653d0e59542f8f
CadRom <romain.cadart@gmail.com>
2018-07-26 11:22:44 +0200

    ajout test Servlet

--- Back/Code/principale/service/src/main/java/group1/service/MaServlet.java
+++ Back/Code/principale/service/src/main/java/group1/service/MaServlet.java
@@ -1,16 +1,30 @@
 package group1.service;
 
-public class Servlet {
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domaine.main.Client;
+import domaine.main.Conseiller;
+
 
 	
 	@WebServlet("/authentification")
-	public class Servlet extends HttpServlet {
+	public class MaServlet extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 	       
 	    /**
 	     * @see HttpServlet#HttpServlet()
 	     */
-	    public Controleur() {
+	    public MaServlet() {
 	        super();
 	        // TODO Auto-generated constructor stub
 	    }
@@ -27,9 +41,9 @@ public class Servlet {
 
 		// Etape 1 : Récupération des paramètres de la requête
 		
-		String login = request.getParameter("usernameInput");
+		/*String login = request.getParameter("usernameInput");
 		String pwd = request.getParameter("passwordInput");
-		
+		*/
 		// Etape 2 : Soumettre les paramètres de la requête à la couche service et récupérer résultat
 		
 			
@@ -41,21 +55,25 @@ public class Servlet {
 		ArrayList<Client> listeClient = clientService.retournerListeClient(conseiller);
 		
 		
-		HttpSession maSession = request.getSession();		
-		maSession.setAttribute("listeClient", listeClient);
+		for (Client client : listeClient) {
+		System.out.println(client);}
+		
+		
+		/*HttpSession maSession = request.getSession();		
+		maSession.setAttribute("listeClient", listeClient);*/
 		
 		// UserService userService = new UserService();
-		// boolean userValid = userService.verifUser (user) ;
+		// boolean userValid = userService.verifUser (user) ;-
 		// maSession.setAttribute("validUser", userValid);
 		
 		// Etape 3 : Réponse à l'utilisateur
-		RequestDispatcher   dispatcher = request.getRequestDispatcher("resultatLogin.jsp");
+		/*RequestDispatcher   dispatcher = request.getRequestDispatcher("resultatLogin.jsp");
 		dispatcher.forward(request, response);
-		
+		*/
 		}
 
 	}
 
-}
+
 
 
---
+++ Back/Code/principale/service/src/main/java/group1/service/test.java
@@ -0,0 +1,25 @@
+package group1.service;
+
+import java.util.ArrayList;
+
+import domaine.main.Client;
+import domaine.main.Conseiller;
+
+public class test {
+
+	public static void main(String[] args) {
+	ConseillerService conseillerService = new ConseillerService();	
+		
+		Conseiller conseiller = conseillerService.recupererConseillerParId(1);
+		
+		ClientService clientService = new ClientService();
+		ArrayList<Client> listeClient = clientService.retournerListeClient(conseiller);
+		
+		
+		for (Client client : listeClient) {
+		System.out.println(client);}
+		
+
+	}
+
+}
