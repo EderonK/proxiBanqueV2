c9a0dc1c7d730b2da5aa2c4b81f0576d40d45055
Jules GOLHEN <julesgolhen@yahoo.fr>
2018-07-26 16:20:02 +0200

    for poule

---
+++ Back/Code/principale/dao/src/main/java/dao/DAOVirement.java
@@ -0,0 +1,100 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import domaine.main.CompteCourant;
+import domaine.main.CompteEpargne;
+import domaine.main.Virement;
+
+public class DAOVirement extends DAOGenerique<Virement>
+{	
+	public DAOVirement()
+	{
+		super("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/proxibanque", "root", "");
+	}
+
+	@Override
+	public String getNomTable()
+	{
+		return "virement";
+	}
+	
+	@Override
+	public String getAttributs()
+	{
+		return "(montant, numCompte, numCompteReceveur)";
+	}
+
+	@Override
+	public Virement traitementLectureElement(ResultSet rs)
+	{
+
+		Virement virement = new Virement();
+		try
+		{
+			virement.setMontant(rs.getInt("montant"));
+			
+			String numCompteDonneur =rs.getString("numCompte");
+			String numCompteReceveur = rs.getString("numCompteReceveur");
+			
+			DAOCompteCourant daoCompteCourant = new DAOCompteCourant();
+			for(CompteCourant compteCourant : daoCompteCourant.toutLireElement())
+			{
+				if(numCompteDonneur.equals(compteCourant.getNumCompte()))
+				{
+					virement.setNumCompte(compteCourant);
+				}
+				
+				if(numCompteReceveur.equals(compteCourant.getNumCompte()))
+				{
+					virement.setNumCompteReceveur(compteCourant);
+				}
+			}
+			
+			DAOCompteEpargne daoCompteEpargne = new DAOCompteEpargne();
+			for(CompteEpargne compteEpargne : daoCompteEpargne.toutLireElement())
+			{
+				if(numCompteDonneur.equals(compteEpargne.getNumCompte()))
+				{
+					virement.setNumCompte(compteEpargne);
+				}
+				
+				if(numCompteReceveur.equals(compteEpargne.getNumCompte()))
+				{
+					virement.setNumCompteReceveur(compteEpargne);
+				}
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return virement;	
+	}
+	
+	@Override
+	public String traitementLectureClefEtrangere(ResultSet rs)
+	{
+		return null;	
+	}
+	
+	@Override
+	public String getValeurs(Virement virement)
+	{
+		return "('" + virement.getMontant() + "', '" + virement.getNumCompte() +  "', '" + virement.getNumCompteReceveur() +")";
+	}
+
+	@Override
+	public String getUpdate(Virement virement)
+	{
+		return "nom = '" + virement.getMontant() + "', prenom =  '" + virement.getNumCompte() + "', clan =  '" + virement.getNumCompteReceveur();
+	}
+
+	@Override
+	public String getNomIdentifiant()
+	{
+		return "";
+	}
+}
