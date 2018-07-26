f0eff0a396d95000b40474bd78830fcfe073cf28
Jules GOLHEN <julesgolhen@yahoo.fr>
2018-07-26 11:44:12 +0200

    ajout IdentificationService fonctionnel

--- Back/Code/principale/service/src/main/java/group1/service/IdentificationService.java
+++ Back/Code/principale/service/src/main/java/group1/service/IdentificationService.java
@@ -2,6 +2,9 @@ package group1.service;
 
 import dao.DAOConseiller;
 import dao.DAOGerant;
+import domaine.main.Conseiller;
+import domaine.main.Gerant;
+import domaine.main.Utilisateur;
 
 public class IdentificationService
 {
@@ -11,11 +14,27 @@ public class IdentificationService
 	IdentificationService()
 	{
 		this.daoConseiller = new DAOConseiller();
-		this.daoGerant = new DAOGerant();
+		//this.daoGerant = new DAOGerant();
 	}
 	
-	verficationMotDePasse(String id, String mdp)
+	Utilisateur verficationMotDePasse(String id, String mdp)
 	{
 		for(Gerant gerant : this.daoGerant.toutLireElement())
-	}
+		{
+			if(gerant.getIdentifiant().equals(id) && gerant.getMotDePasse().equals(mdp))
+			{
+				return gerant;
+			}		
+		}
+		
+		for(Conseiller conseiller : this.daoConseiller.toutLireElement())
+		{
+			if(conseiller.getIdentifiant().equals(id) && conseiller.getMotDePasse().equals(mdp))
+			{
+				return conseiller;
+			}
+		}
+		
+		return null;
+	}	
 }
